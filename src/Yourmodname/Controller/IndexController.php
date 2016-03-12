48859930f73848aa09b2a5cd6ba966452505992f
Björn Bartels <bba@p-ad.de>
2016-03-12 17:30:01 +0100

    update doc info

--- src/Yourmodname/Controller/IndexController.php
+++ src/Yourmodname/Controller/IndexController.php
@@ -8,7 +8,7 @@
  * @package		BB's Zend Framework 2 Components
  * @package		CRUD Module Template
  * @author		Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
- * @link		http://gitlab.dragon-projects.de:81/groups/zf2
+ * @link		https://gitlab.bjoernbartels.earth/groups/zf2
  * @license		http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @copyright	copyright (c) 2016 Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
  */
