008c1037f31dc318e392a10eef6bdf8e9b630c18
Björn Bartels <bba@p-ad.de>
2016-04-09 02:08:29 +0200

    update doc info

--- src/Yourmodname/Controller/IndexController.php
+++ src/Yourmodname/Controller/IndexController.php
@@ -4,13 +4,13 @@
  * 
  * CRUD Module Template
  *
- * @package        [MyApplication]
- * @package        BB's Zend Framework 2 Components
- * @package        CRUD Module Template
- * @author        Björn Bartels <development@bjoernbartels.earth>
+ * @package     [MyApplication]
+ * @subpackage  BB's Zend Framework 2 Components
+ * @subpackage  CRUD Module Template
+ * @author      Björn Bartels <coding@bjoernbartels.earth>
  * @link        https://gitlab.bjoernbartels.earth/groups/zf2
- * @license        http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
- * @copyright    copyright (c) 2016 Björn Bartels <development@bjoernbartels.earth>
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @copyright   copyright (c) 2016 Björn Bartels <coding@bjoernbartels.earth>
  */
 
 namespace Yourmodname\Controller;
